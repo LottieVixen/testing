7ceba7f362e7c891be62cd0e3d830fd3735e092d
ritzip <ritzip.rit@gmail.com>
2020-06-23 14:59:05 +0800

    inode

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,31 @@
+//infoNode
+const iNode = extendContent(PowerNode, "info-node", {
+    setBars(){
+        this.super$setBars();
+
+        this.bars.add("power-i", func(entity => new Bar(
+            prov(() => "N/P: " + 
+                (entity.power.graph.getPowerNeeded() > 0 ? "-" : "") + Strings.fixed(entity.power.graph.getPowerNeeded() * (60 / entity.delta()), 1) + "/" + 
+                (entity.power.graph.getPowerProduced() > 0 ? "+" : "") + Strings.fixed(entity.power.graph.getPowerProduced() * (60 / entity.delta()), 1)
+            ),
+            prov(() => Pal.powerBar),
+            floatp(() => entity.power.graph.getSatisfaction())
+        )));
+    }
+});
+
+iNode.health = 1;
+iNode.maxNodes = 99;
+iNode.buildVisibility = BuildVisibility.sandboxOnly;
+iNode.requirements = [new ItemStack(Items.copper, 1)];
+iNode.size = 1;
+iNode.update = true;
+iNode.localizedName = "Info Node";
+iNode.description = "";
+
 //dpsBlock
 var noPierce = false;
-const dpsUnit = UnitType("dps-unit", prov(a => extend(GroundUnit, {
+const dpsUnit = new UnitType("dps-unit", prov(a => extend(GroundUnit, {
     //this.spawner = bad
 
     _owner: null,
@@ -34,6 +59,7 @@ const dpsUnit = UnitType("dps-unit", prov(a => extend(GroundUnit, {
         if(b.getBulletType().pierce && noPierce) b.remove();
     }
 })));
+
 dpsUnit.drag = 1;
 dpsUnit.speed = 0;
 dpsUnit.maxVelocity = 0;
